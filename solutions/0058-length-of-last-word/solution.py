907f88e940f50ff22b06df47b6b7d1647427b10d
Ayush-kr-007 <ak00945367@gmail.com>
2026-06-09 17:59:51 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 12.6 MB (8.48%)

---
+++ solutions/0058-length-of-last-word/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        n = len(s)
+
+        i = -1
+        while i>=(-1*n) and s[i]!=" ":
+            i-=1
+        i+=1
+        i*=-1
+
+        return i 
