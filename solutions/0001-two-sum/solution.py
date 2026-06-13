605052171bba59a57a26669f8eb06141b28982ae
Ayush-kr-007 <ak00945367@gmail.com>
2026-06-13 14:50:50 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 13.2 MB (53.18%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -5,23 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # Create a dictionary to store numbers and their indices
-        num_to_index = {}
+        n = len(nums)
+        dict1={}
+        for i in range(n):
+            rem =target-nums[i]
+            if rem in dict1:
+                return [dict1[rem],i]
+            
+            dict1[nums[i]]=i
 
-        # Iterate through the list of numbers
-        for i, num in enumerate(nums):
-            # Calculate the complement
-            complement = target - num
-
-            # Check if the complement is already in the dictionary
-            if complement in num_to_index:
-                # If found, return the indices as a list
-                return [num_to_index[complement], i]
-
-            # Otherwise, add the current number and its index to the dictionary
-            num_to_index[num] = i
-
-        # If no solution is found, return an empty list
-        # (Though in this problem, it’s guaranteed to have one solution)
-        return []
 
